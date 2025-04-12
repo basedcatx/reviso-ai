8b3ef458516f7f91e74052f11c1ba6fdaf4c2751
OG BaseDCaTx <tazuhf@gmail.com>
2025-04-12 19:37:49 +0100

    initial commit fixed

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -19,7 +19,7 @@ const ImageCard = ({
         "rounded-md bg-blue-50 border flex justify-center items-center p-6 gap-4 flex-col shadow-sm"
       }
     >
-      <Image src={icon} alt={"image"} width={100} height={200} />
+      <Image src={icon} alt={"image"} width={100} height={200} unoptimized />
       <h1 className={"font-bold text-4xl text-center"}>{title}</h1>
       <p className={"text-center font-light text-lg"}>{desc}</p>
     </div>
