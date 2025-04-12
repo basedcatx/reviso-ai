76cb11c11d15d958b83867dc03635643b37dbdc6
OG BaseDCaTx <tazuhf@gmail.com>
2025-04-12 19:16:56 +0100

    initial commit fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,149 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ReactNode } from "react";
-import { Montserrat, Nunito } from "@next/font/google";
+import localFont from "next/font/local";
 
-const nunito = Nunito({
-  subsets: ["latin"],
+const nunito = localFont({
+  src: [
+    {
+      path: "/fonts/Nunito/Nunito-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-ExtraLightItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-Bold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-BoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-ExtraBold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-ExtraBoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-Black.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Nunito/Nunito-BlackItalic.ttf",
+      weight: "800",
+      style: "italic",
+    },
+  ],
+
   variable: "--font-nunito",
 });
 
-const montserrat = Montserrat({
-  subsets: ["latin"],
+const montserrat = localFont({
+  src: [
+    {
+      path: "/fonts/Montserrat/Montserrat-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-ExtraLightItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-Bold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-BoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-ExtraBold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-ExtraBoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-Black.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Montserrat/Montserrat-BlackItalic.ttf",
+      weight: "800",
+      style: "italic",
+    },
+  ],
+
   variable: "--font-montserrat",
 });
 
--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import Image from "next/image";
 
 const ImageCard = ({
